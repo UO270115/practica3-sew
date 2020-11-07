03766f101de2a5510eca73828a3373c3616faea7
uo270115 <uo270115@uniovi.es>
2020-11-07 13:01:52 +0100

    Correcciones

--- CalculadoraBasica.js
+++ CalculadoraBasica.js
@@ -1,10 +1,9 @@
 "use strict";
 class CalculadoraBasica{
-    // m- te resta de memoria lo que tienes en pantalla
-    // m+ te suma ""
     
     constructor(){
         this.resultado = "";
+        this.mostrar = "";
         this.memoria = 0;
     }
 
@@ -12,17 +11,21 @@ class CalculadoraBasica{
     mrc(){
         try { 
             this.resultado = this.memoria;
-            document.getElementById("resultado").value = this.resultado;
+            this.mostrar = this.resultado;
+            document.getElementById("resultado").value = this.mostrar;
         }
         catch(err) {
             document.getElementById("resultado").value = "Error = " + err;
         }
     }
 
+    // m- te resta de memoria lo que tienes en pantalla
+    // m+ te suma ""
     mMenos(){
         try { 
             this.memoria -= parseInt(document.getElementById("resultado").value);
             this.resultado = this.memoria;
+            this.mostrar = this.resultado;
         }
         catch(err) {
              document.getElementById("resultado").value = "Error = " + err;
@@ -33,6 +36,7 @@ class CalculadoraBasica{
         try { 
             this.memoria += parseInt(document.getElementById("resultado").value);
             this.resultado = this.memoria;
+            this.mostrar = this.resultado;
         }
         catch(err) {
              document.getElementById("resultado").value = "Error = " + err;
@@ -41,43 +45,51 @@ class CalculadoraBasica{
 
     dividir(){
         this.resultado += "/";
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar += "/";
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     digitos(digito){
         this.resultado += digito;
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar += digito;
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     multiplicación(){
         this.resultado += "*";
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar += "*";
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     resta(){
         this.resultado += "-";
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar += "-";
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     suma(){
         this.resultado += "+";
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar += "+";
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     punto(){
         this.resultado += ".";
-        document.getElementById("resultado").value = this.resultado;
+        this.memoria += ".";
+        document.getElementById("resultado").value = this.mostar;
     }
 
     borrar(){
         this.resultado = "";
-        document.getElementById("resultado").value = this.resultado;
+        this.mostrar = "";
+        document.getElementById("resultado").value = this.mostrar;
     }
 
     igual(){
         try { 
-            document.getElementById("resultado").value = eval(this.resultado);
-            this.resultado = document.getElementById("resultado").value;
+            this.mostrar = eval(this.resultado);
+            document.getElementById("resultado").value = this.mostrar
+            this.resultado = this.mostrar;
         }
         catch(err) {
              document.getElementById("resultado").value = "Error = " + err;
